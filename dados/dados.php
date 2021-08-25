947923e6a8cf113ee23e07823bce015661a2e5dd
LuisEduardoReif <89509148+LuisEduardoReif@users.noreply.github.com>
2021-08-25 20:55:27 -0300

    Add files via upload

---
+++ dados/dados.php
@@ -0,0 +1,38 @@
+<?php
+  
+  $produtos =[
+    [
+      'id' => 1,
+      'nome' => 'Pacote de 400g de bala de goma sortida',
+      'imagem' => './images/gomas.jpg',
+      'preco' => '24,99',
+      'descricao' => 'A mais deliciosa bala de goma. pra lembar dos bons tempos ',
+      'ingredientes' => [
+        'Açúcar, xarope de glicose, amido de milho modificado, aromatizantes, acidulante ácido cítrico e corantes artificiais amarelo tartrazina, amarelo crepúsculo, vermelho 40 e azul brilhante. Não contém glúten. ',
+        
+      ]
+    ],
+    [
+      'id' => 2,
+      'nome' => 'Pirulito de chocolate recheado',
+      'imagem' => './images/pirulito.jpg',
+      'preco' => '9,99',
+      'descricao' => 'O melhor e mais saboroso pirulito feito de chocolate recheado de chocolate branco. Edição limitada.',
+      'ingredientes' => [
+        'Açúcar,chocolate preto,leite,chocolate branco.'
+        
+      ]
+    ],
+    [
+      'id' => 3,
+      'nome' => 'Trufas varios sabores',
+      'imagem' => './images/trufas.jpg',
+      'preco' => '4,99',
+      'descricao' => 'Quem não adora trufas, não é mesmo? De varios sabores e rechios!',
+      'ingredientes' => [
+        'manteiga de cacau , leite integral em pó, massa de cacau, creme de leite em pó, soro de leite em pó, edulcorante maltitol, agente de corpo polidextrose, emulsificantes: lecitina de soja e ésteres de ácido ricinoleico interesterificados com poliglicerol e recheio trufa ao leite',
+        
+      ]
+    ]
+  ]
+?>
