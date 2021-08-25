6ccd783a41763bfbc2c5551d40a5d442fc5150bd
LuisEduardoReif <89509148+LuisEduardoReif@users.noreply.github.com>
2021-08-25 20:54:46 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include('pages/header.php'); ?>
+<header class="banner">
+  <div class="container">
+    <div class="banner-content">
+      <h1 class="banner-title">Venha conhecer a melhor banca de doces que voçe vai ver Hoje!</h1>
+      <a class="btn" href="#quem_somos">SAIBA MAIS</a>
+    </div>
+  </div>
+</header>
+
+<?php include('pages/produtos.php'); ?>
+
+<?php include('pages/quem_somos.php'); ?>
+
+<?php include('pages/contato.php'); ?>
+
+<?php include('pages/footer.php'); ?>
