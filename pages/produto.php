8be77324469b15d295861ab8af81483f2d6f0e2e
LuisEduardoReif <chacaldufloripa@gmail.com>
2021-08-25 21:07:05 -0300

    Update produto.php

--- pages/produto.php
+++ pages/produto.php
@@ -4,7 +4,7 @@
   <section class="pagina-produto container">
     <div class="img-produto">
       <img src="<?=$produtos[$_GET['id'] - 1]['imagem']?>" alt="">
-    </div>
+  </div>
     <div class="info-produto">
       <h1 class="info-titulo-produto"><?=$produtos[$_GET['id'] - 1]['nome']?></h1>
       <p class="info-descricao-produto"><?=$produtos[$_GET['id'] - 1]['descricao']?></p>
